1962036a87e1a694ad4ced3b197e7c838d0c0075
CoolStar <coolstarorganization@gmail.com>
2015-10-12 07:21:46 -0700

    Change buildversion. Patch seabios to speed up boot time.

--- scripts/buildversion.sh
+++ scripts/buildversion.sh
@@ -11,7 +11,7 @@ elif [ -f .version ]; then
 else
     VERSION="?"
 fi
-VERSION="${VERSION}-`date +"%Y%m%d_%H%M%S"`-`hostname`"
+VERSION="${VERSION}-`date +"%Y%m%d_%H%M%S"`-coolstar"
 echo "Version: ${VERSION}"
 
 # Build header file
